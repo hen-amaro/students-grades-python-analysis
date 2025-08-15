db8558199989194c489061e5016bf83c0602aeee
Henrique <henamaropc@gmail.com>
2025-08-15 14:32:42 -0300

    Trata os dados e converte as notas para o sistema númerico

--- src/main.py
+++ src/main.py
@@ -1,8 +1,44 @@
 import pandas as pd
 
-#Lê o arquivo CSV
 
+#Tabela de conversão
+grade_map = {
+    'A+': 4.0,
+    'A': 4.0,
+    'A-': 3.7,
+    'B+': 3.3,
+    'B': 3.0,
+    'B-': 2.7,
+    'C+': 2.3,
+    'C': 2.0,
+    'C-': 1.7,
+    'D+': 1.3,
+    'D': 1.0,
+    'F': 0.0
+}
+
+
+
+#Lê o arquivo CSV
 dados = pd.read_csv("data/raw/Grades.csv")
+
+cols_grade = dados.columns[1:-1]
+
+#Aplicando o mapeamento das notas
+for col in cols_grade:
+    dados[col] = dados[col].map(grade_map)
+
+#Salvar dados tratados
+dados.to_csv("data/processed/grades_numeric.csv", index=False)
+
+
+
+
+
+
+
+
+
 
 #Calcula a média da coluna CGPA
 
